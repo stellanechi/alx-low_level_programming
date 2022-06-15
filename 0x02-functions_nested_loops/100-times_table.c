b81ba206d8644be5996b256ccd09d372865887e5
stellanechi <stellanechi65@gmail.com>
2022-06-15 07:58:44 -0700

    Advance task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,32 +20,32 @@ void print_times_table(int n)
 				c = a * b;
 				d = c / 100;
 				e = (c / 10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0'); }
-				else if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				else
-				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-					_putchar('\n'); }}}}													
+					f = (c % 100) % 10;
+					if (b == 0)
+					{
+						_putchar('0'); }
+					else if (c < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + f);
+					}
+					else if (c < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + e);
+						_putchar('0' + f);
+					}
+					else
+					{
+						_putchar('0' + d);
+						_putchar('0' + e);
+						_putchar('0' + f);
+					}
+					if (b < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					else
+						_putchar('\n'); }}}}																							
